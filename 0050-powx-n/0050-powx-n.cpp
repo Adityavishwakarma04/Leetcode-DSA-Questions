e305f576b44aa4e4a26bc8679e551516b07d8940
Aditya vishwakarma <101093864+Adityavishwakarma04@users.noreply.github.com>
2024-06-24 23:04:03 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (21.56%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    double solve(double x, long n){
+        
+        if(n==0) return 1;
+        if(x==0) return 0;
+        
+        if(n<0) return solve(1/x, -n);
+        if(n%2==0) return solve(x*x, n/2);
+        else return x*(solve(x*x, (n-1)/2));
+    }
+    
+    
+    double myPow(double x, int n) {
+        
+        return solve(x, (long)n);
+        
+        
+    }
+};
