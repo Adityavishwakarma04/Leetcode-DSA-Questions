2a0c805a805793bb1e16a17b9879156a192e3004
Aditya vishwakarma <101093864+Adityavishwakarma04@users.noreply.github.com>
2024-07-05 10:35:56 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (6.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -6,27 +6,19 @@ public:
         long long end=x;
         int ans=0;
         long long mid=start+(end-start)/2;
-        
         while(start<=end){
-            
           mid=start+(end-start)/2;
-            
             if((mid*mid)==x){
                 return mid;
             }
-            
-            else if((mid*mid)>x){
-                
+            else if((mid*mid)>x){ 
                end=mid-1;
             }
-            
             else{
                 ans=mid;
                 start=mid+1;
             }
         }
-        
         return ans;
-        
     }
 };
